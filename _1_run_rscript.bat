29d2b5c4abb12d3bfdbfd4f7ce538527da16f8b0
Anh Hoang Duc <hoangducanh89@gmail.com>
2019-06-28 13:46:05 +0700

    Update website

---
+++ _1_run_rscript.bat
@@ -0,0 +1,7 @@
+@echo off
+rem set "dest=C:\Users\anhhd3\Desktop\99.test\13_Tong hop SQL\02.SQL website"
+rem echo %dest%
+rem "C:\Program Files\R\R-3.4.0\bin\x64\Rscript.exe" "%dest%\create_site.R"
+"C:\Program Files\R\R-3.4.0\bin\x64\Rscript.exe" "%cd%\create_site.R"
+rem pause
+msg * Finish your website!
---
+++ _2_git_push.bat
@@ -0,0 +1,6 @@
+cd %cd%
+git add .
+git commit -m "Update website"
+git push
+
+msg * Finish your website!
